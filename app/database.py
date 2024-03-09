040b31e561f807ffe5e43698427d507ba5cfd56c
erikdeirdre <pwhite@delpwhite.org>
2024-03-09 16:58:50 -0500

    add tables back

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 SQLALCHEMY_DATABASE_URL = environ.get("SQLALCHEMY_DATABASE_URL")
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, echo=True
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
